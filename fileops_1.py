eabcfa7f0cad6e1c8d6497f4e719e6fa6ddd3819
dinesh <dineshk1706@gmail.com>
2019-06-02 19:54:15 -0500

    modified changes to fileops.py

--- fileops_1.py
+++ fileops_1.py
@@ -1,12 +1,14 @@
 import os
 import random
 import string
-#i = 0
+import datetime
+from datetime import date, time
 count = 1
 file_dir = "/home/dinesh/PycharmProjects/random/"
 
 while count <= 10:
-    file_name = "random_{}.txt".format(count)
+    date_file = datetime.datetime.now().strftime("%d_%m_%y_%H_%M_%S")
+    file_name = "file_{}".format(date_file)
     i = 0
     while i < 104857601:
         with open("{}{}".format(file_dir, file_name), "a") as file:
